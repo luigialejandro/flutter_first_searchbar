1859e6c9d6fe26588250484abde2d7520d8e8507
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-02-07 22:48:52 -0600

    Adding searchbar functionality overriding methods of SearchDelegate class. The items that form the list of suggestions are string countries in this particular case

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,123 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('Search'),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: MySearchDelegate(),
+                );
+              },
               icon: const Icon(Icons.search),
             ),
           ],
         ),
       );
+}
+
+class MySearchDelegate extends SearchDelegate {
+  List<String> searchResults = [
+    'Algeria',
+    'Argentina',
+    'Belgium',
+    'Bolivia',
+    'Brazil',
+    'Cameroon',
+    'Canada',
+    'Chile',
+    'China',
+    'Colombia',
+    'Costa Rica',
+    'Croatia',
+    'Denmark',
+    'El Salvador',
+    'Egypt',
+    'Ethiopia',
+    'Finland',
+    'France',
+    'Germany',
+    'Greece',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Ireland',
+    'Italy',
+    'Japan',
+    'Mexico',
+    'Morocco',
+    'Netherlands',
+    'Nicaragua',
+    'Nigeria',
+    'Norway',
+    'Panama',
+    'Poland',
+    'Portugal',
+    'Romania',
+    'Russia',
+    'Senegal',
+    'Singapore',
+    'South Africa',
+    'South Korea',
+    'Spain',
+    'Sweden',
+    'Switzerland',
+    'Thailand',
+    'UK',
+    'Ukraine',
+    'Uruguay',
+    'USA',
+    'Vietnam',
+  ];
+
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () => close(context, null),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = "";
+            }
+          },
+        ),
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) => Container();
+  //Center(
+  //  child: Text(query, style: const TextStyle(fontSize: 64)),
+  //);
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = searchResults.where((searchResult) {
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+
+      return result.contains(input);
+    }).toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
 }
